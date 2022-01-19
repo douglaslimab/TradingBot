3dcc0b5080de7dafa0e6b971af59bd594b32cd62
douglaslimab <douglaslimab@gmail.com>
2022-01-19 00:58:21 +0000

    yahoo api

--- alphaAPI.js
+++ alphaAPI.js
@@ -2,12 +2,18 @@
 var request = require('request');
 
 var funct = 'TIME_SERIES_DAILY'
-var symbol = 'egie3'
+var symbol = 'wege3'
 var interval = '5min'
 var apikey = '0RG6KZV5T50KVDL7'
 
 // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-var url = `https://www.alphavantage.co/query?function=${funct}&symbol=${symbol}.SA&apikey=${apikey}`;
+//var url = `https://www.alphavantage.co/query?function=${funct}&symbol=${symbol}.SA&apikey=${apikey}`;
+
+var symbol = 'itsa4'
+var interval = '60m'  //Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
+var range = '5d'      //"validRanges":["1d","5d","1mo","3mo","6mo","1y","2y","5y","10y","ytd","max"]
+
+var url = `https://query1.finance.yahoo.com/v8/finance/chart/${symbol}.SA?region=US&lang=en-US&includePrePost=false&interval=${interval}&useYfid=true&range=${range}&corsDomain=finance.yahoo.com&.tsrc=finance`
 
 request.get({
     url: url,
@@ -20,6 +26,11 @@ request.get({
       console.log('Status:', res.statusCode);
     } else {
       // data is successfully parsed as a JSON object:
-      console.log(data);
+      console.log(JSON.stringify(data.chart.result[0].timestamp));
+      console.log(JSON.stringify(data.chart.result[0].indicators.quote[0].open));
+      console.log(JSON.stringify(data.chart.result[0].indicators.quote[0].high));
+      console.log(JSON.stringify(data.chart.result[0].indicators.quote[0].low));
+      console.log(JSON.stringify(data.chart.result[0].indicators.quote[0].close));
+      console.log(JSON.stringify(data.chart.result[0].indicators.quote[0].volume));
     }
 });
--- index.js
+++ index.js
@@ -7,7 +7,13 @@ var CronJob = require('cron').CronJob
 const app = express()
 url = 'https://www.infomoney.com.br/cotacoes/engie-brasil-egie3/'
 
-var job = new CronJob('*/12 * * * * *', function(){
+//https://www.infomoney.com.br/cotacoes/mdiasbranco-mdia3/
+//https://www.infomoney.com.br/cotacoes/itausa-itsa4/
+//https://www.infomoney.com.br/cotacoes/taesa-taee11/
+//https://www.infomoney.com.br/cotacoes/weg-wege3/
+//https://www.infomoney.com.br/cotacoes/grendene-grnd3/
+
+var job = new CronJob('*/15 * * * * *', function(){
     axios(url)
         .then(response => {
             const html = response.data
